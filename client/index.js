0ffe099d2468dfe46fbecf14b414a46bce0af30b
CatalaHD <thecatalahd@gmail.com>
2021-05-02 15:11:02 +0200

    new style

--- client/index.js
+++ client/index.js
@@ -4,85 +4,84 @@ const readline = require('readline');
 const red = chalk.bold.red;
 const green = chalk.bold.green;
 
-const link = "http://localhost:3000";
+const link = 'http://localhost:3000';
 
 // Get data from the server
 async function getData() {
-    await fetch(`${link}/info`)
-        .then(res => res.json())
-        .then((data) => {
-            console.log(data);
-        })
-        .catch((err) => console.log(red("Server is offline!")));
+  await fetch(`${link}/info`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => console.log(red('Server is offline!')));
 }
 
 // Post data to the server
 async function postData(body) {
-    await fetch(`${link}/add`, {
-            method: 'POST',
-            body: JSON.stringify(body),
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        })
-        .then(res => res.json())
-        .then((data) => {
-            console.log(data);
-        }).catch((err) => console.log(red("Server is offline!")));
+  await fetch(`${link}/add`, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => console.log(red('Server is offline!')));
 }
 
 const showMenu = function () {
-    console.log(green("\nMENU:"));
-    console.log("0.- Exit");
-    console.log("1.- Get TODOs from the server");
-    console.log("2.- Add new TODO to the server\n");
+  console.log(green('\nMENU:'));
+  console.log('0.- Exit');
+  console.log('1.- Get TODOs from the server');
+  console.log('2.- Add new TODO to the server\n');
 };
 
 let code;
 
 var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 function readCodeAndExecute() {
-    showMenu();
+  showMenu();
 
-    rl.question('Enter the code of the action: ', async (answer) => {
-        if (isNaN(answer)) {
-            console.log(red("The code is not a number!"));
-            readCodeAndExecute();
-            return;
-        }
-        code = parseInt(answer);
+  rl.question('Enter the code of the action: ', async (answer) => {
+    if (isNaN(answer)) {
+      console.log(red('The code is not a number!'));
+      readCodeAndExecute();
+      return;
+    }
+    code = parseInt(answer);
 
-        if (code === 1) {
-            await getData();
-            readCodeAndExecute();
-        } else if (code === 2) {
-            const todo = {
-                name: "??",
-                description: "??"
-            };
+    if (code === 1) {
+      await getData();
+      readCodeAndExecute();
+    } else if (code === 2) {
+      const todo = {
+        name: '??',
+        description: '??',
+      };
 
-            rl.question("Name: ", (answer) => {
-                todo.name = answer;
-                rl.question("Description: ", async (answer) => {
-                    todo.description = answer;
-                    await postData(todo);
-                    readCodeAndExecute();
-                });
-            });
-
-        } else if (code === 0) {
-            console.log("Bye! :)");
-            rl.close();
-        } else {
-            console.log(red("The code is not in the range. See the menu!"));
-            readCodeAndExecute();
-        }
-    });
+      rl.question('Name: ', (answer) => {
+        todo.name = answer;
+        rl.question('Description: ', async (answer) => {
+          todo.description = answer;
+          await postData(todo);
+          readCodeAndExecute();
+        });
+      });
+    } else if (code === 0) {
+      console.log('Bye! :)');
+      rl.close();
+    } else {
+      console.log(red('The code is not in the range. See the menu!'));
+      readCodeAndExecute();
+    }
+  });
 }
-
 
 readCodeAndExecute();
--- src/index.js
+++ src/index.js
@@ -10,41 +10,38 @@ app.set('port', process.env.PORT || 3000);
 
 // Middlewares
 app.use(morgan('dev'));
-app.use(express.urlencoded({
-    extended: false
-}));
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 app.use(express.json());
 
 // Routes
-app.get("/", (_req, res) => {
-    res.json("Mejor accede a /info");
+app.get('/', (_req, res) => {
+  res.json('Mejor accede a /info');
 });
 
-app.get("/info", (_req, res) => {
-    res.send(db.todos);
+app.get('/info', (_req, res) => {
+  res.send(db.todos);
 });
 
-app.post("/add", (req, res) => {
+app.post('/add', (req, res) => {
+  const { name, description } = req.body;
 
-    const {
-        name,
-        description
-    } = req.body;
-
-    if (name && description) {
-        db.todos.push({
-            name: name,
-            description: description
-        });
-        fs.writeFileSync('data/db.json', JSON.stringify(db));
-        res.json("Succeeded! Todo added to database.");
-    } else {
-        res.json("Request denied. Fix format.");
-    }
-
+  if (name && description) {
+    db.todos.push({
+      name: name,
+      description: description,
+    });
+    fs.writeFileSync('data/db.json', JSON.stringify(db));
+    res.json('Succeeded! Todo added to database.');
+  } else {
+    res.json('Request denied. Fix format.');
+  }
 });
 
 // Server
 app.listen(app.get('port'), () => {
-    console.log(`El servidor está escuchando en el puerto ${app.get('port')}!`);
+  console.log(`El servidor está escuchando en el puerto ${app.get('port')}!`);
 });
